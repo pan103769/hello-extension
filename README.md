45027f702905c2eb6ba761991245b048c8bf94e2
Amartya Ayush <pan324790@gmail.com>
2026-07-14 20:26:14 +0530

    Add extension icon

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hello Extension
+
+My first Chrome extension.
+
+## Features
+
+- Popup UI
+- Manifest V3
+- HTML
+- CSS
+- JavaScript
+
--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
 console.log("HELOLOLOLOHello Pant! 🚀")
+document.querySelector("button").addEventListener("click",counter_function)
+var i = 0
+function counter_function(){
+    i ++ 
+    document.querySelector("p").innerHTML="counter: ["+i+"]"
+}
